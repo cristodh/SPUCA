e4162e69cdcbddc86785b6aa53d08e4d5eb2487f
cristodh <cdelgado.hidalgo1996fwd@gmail.com>
2025-08-29 11:33:52 -0600

    new msjs

---
+++ public/services/fetch.js
@@ -0,0 +1,170 @@
+/*Estudiante*/
+async function postDataEst(obj) {
+    try {
+        const peticion = await fetch("http://localhost:2929/estudiantes", {
+            method: "POST",
+            headers: {"Content-Type": "application/json" },
+            body: JSON.stringify(obj)
+        });
+        const respuesta = await peticion.json();
+        return respuesta;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+async function putDataEst(obj) {
+    try {
+        const peticion = await fetch(`http://localhost:2929/estudiantes/${obj.id}`, {
+            method: "PATCH",
+            headers: {"Content-Type": "application/json" },
+            body: JSON.stringify(obj)
+        });
+        const respuesta = await peticion.json();
+        return respuesta;
+    } catch (error) {
+        console.log(error);        
+    }
+}
+
+async function getDataEst() {
+    try {
+        const peticion = await fetch("http://localhost:2929/estudiantes", {
+        method: "GET",
+        headers:{"Content-Type": "application/json" },
+        })
+    const respuesta = await peticion.json();
+    return respuesta;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+async function deleteDataEst(obj) {
+    try {
+        const peticion = await fetch(`http://localhost:2929/estudiantes/${obj}`, {
+            method: "DELETE",
+            headers: {"Content-Type": "application/json" },
+        });
+        const respuesta = await peticion.json();
+        return respuesta;
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
+
+/*Docente*/
+
+async function postDataDoc(obj) {
+    try {
+        const peticion = await fetch("http://localhost:2929/docentes", {
+            method: "POST",
+            headers: {"Content-Type": "application/json" },
+            body: JSON.stringify(obj)
+        });
+        const respuesta = await peticion.json();
+        return respuesta;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+async function putDataDoc(obj) {
+    try {
+        const peticion = await fetch(`http://localhost:2929/docentes/${obj.id}`, {
+            method: "PATCH",
+            headers: {"Content-Type": "application/json" },
+            body: JSON.stringify(obj)
+        });
+        const respuesta = await peticion.json();
+        return respuesta;
+    } catch (error) {
+        console.log(error);        
+    }
+}
+
+async function getDataDoc() {
+    try {
+        const peticion = await fetch("http://localhost:2929/docentes", {
+        method: "GET",
+        headers:{"Content-Type": "application/json" },
+        })
+    const respuesta = await peticion.json();
+    return respuesta;
+    } catch (error) {
+        console.log(error);
+    }
+}
+async function deleteDataDoc(obj) {
+    try {
+        const peticion = await fetch("http://localhost:2929/docentes/${obj}",{
+            method: "DELETE",
+            headers: {"Content-Type": "application/json" },
+        });
+        const respuesta = await peticion.json();
+        return respuesta;
+    }  catch (error) {
+        console.log(error);
+    }
+}
+
+/*CPU*/
+
+async function postDataCpu(obj) {
+    try {
+        const peticion = await fetch("http://localhost:2929/cpu", {
+            method: "POST",
+            headers: {"Content-Type": "application/json" },
+            body: JSON.stringify(obj)
+        });
+        const respuesta = await peticion.json();
+        return respuesta;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+async function putDataCpu(obj) {
+    try {
+        const peticion = await fetch(`http://localhost:2929/cpu/${obj.id}`, {
+            method: "PATCH",
+            headers: {"Content-Type": "application/json" },
+            body: JSON.stringify(obj)
+        });
+        const respuesta = await peticion.json();
+        return respuesta;
+    } catch (error) {
+        console.log(error);        
+    }
+}
+
+async function getDataCpu() {
+    try {
+        const peticion = await fetch("http://localhost:2929/cpu", {
+        method: "GET",
+        headers:{"Content-Type": "application/json" },
+        })
+    const respuesta = await peticion.json();
+    return respuesta;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+async function deleteDataCpu(obj) {
+    try {
+        const peticion = await fetch(`http://localhost:2929/cpu/${obj}`, {
+            method: "DELETE",
+            headers: {"Content-Type": "application/json" },
+        });
+        const respuesta = await peticion.json();
+        return respuesta;
+    }  catch (error) {
+        console.log(error);
+    }
+}
+
+export { postDataEst, putDataEst, getDataEst, deleteDataEst,
+         postDataDoc, putDataDoc, getDataDoc, deleteDataDoc,
+         postDataCpu, putDataCpu, getDataCpu, deleteDataCpu };
