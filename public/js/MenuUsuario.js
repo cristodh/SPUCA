9484bb262af9b1b7a05ab6a806e97d237b5cdfbe
cristodh <cdelgado.hidalgo1996fwd@gmail.com>
2025-08-29 15:46:22 -0600

    new msjs

---
+++ public/js/MenuUsuario.js
@@ -0,0 +1,22 @@
+/* comenzar con metodo fetch http (post, put, delete, get)
+solo un fetch para cada uno
+js
+const botones
+db.json
+
+************
+base 64 - foto de perfil
+
+*/
+import {postDataEst, putDataEst, getDataEst, deleteDataEst,
+        postDataDoc, putDataDoc, getDataDoc, deleteDataDoc,
+        postDataCpu, putDataCpu, getDataCpu, deleteDataCpu} from '../services/fetch.js';
+
+        const logout = document.getElementById('logout');
+        const nombreAdmin = document.getElementById('nombreAdmin');
+        const role = document.getElementById('role');
+        const correo = document.getElementById('correo');
+        const sede = document.getElementById('sede');
+        const imagenPerfil = document.getElementById('imagenPerfil');
+        
+        
